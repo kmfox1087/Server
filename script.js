3ef88859a3eaa8a0fa474238997a4e61d77fc5ae
kmfox1087 <kmfox1087@gmail.com>
2023-05-06 09:10:59 -0400

    added methods to add divs where weather information will go

--- script.js
+++ script.js
@@ -95,10 +95,54 @@ var weatherHTML = function (city, uv) {
     cleaningElement(containerForecast);
 
 //Current city
+//Div for city, date and weather cond.
 var ctn1 = document.createElement("div");
+//Class from bootstrap
 ctn1.classList.add("col-6");
 var ctn2 = document.createElement("div");
 ctn2.classList.add("col-6");
+
+var cityEl = document.createElement("h2");
+var imageCurrent = document.createElement("img");
+
+cityEl.textContent = city + " (" + weatherCondition[0],dateT +")";
+imageCurrent.setAttribute("src", weatherCondition[0].icon);
+
+//Class from bootstrap
+imageCurrent.classList.add("bg-info");
+ctn1.appendChild(cityEl);
+ctn2.appendChild(imageCurrent);
+
+//Div for humidty, wind speed, UV index & temp
+var ctn3 = document.createElement("div");
+ctn3.classList.add("col-12");
+ctn3.innerHTML = "<p>Temperature: " + weatherCondition[0].temp + " °F / " + converTemp(weatherCondition[0].temp) + " °C</p>" + 
+        "<p>Humidity: " + weatherCondition[0].humidity + "% </p>" +
+        "<p>Wind Speed: " + weatherCondition[0].speed + " MPH / " + convertWSpeed(weatherCondition[0].speed) + " KPH </p>" +
+        "<p>UV index: <span class='text-white "+ findUV(uv) + "'>" + uv + "</span></p>";
+
+containerCurrent.appendChild(ctn1);
+containerCurrent.appendChild(ctn2);
+containerCurrent.appendChild(ctn3);
+
+//5 Day Forecast
+
+//Container to store the header for h2
+var ctn6 = document.createElement("div");
+ctn6.classList.add("row");
+var ctn7 = document.createElement("div");
+ctn7.classList.add("col-12");
+ctn6.appendChild(ctn7);
+containerForecast.appendChild(ctn6);
+
+//Container to store the card weather
+var ctn8 = document.createElement("div");
+ctn8.classList.add("d-flex");
+
+//For loop to get the info about the weather stored in the array weatherCond
+for(var i=1; i < weatherCondition.length; i++) {
+    
+}
 }
 
 //Function to start everything - commented out for now
